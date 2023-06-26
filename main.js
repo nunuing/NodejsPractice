202dfcfbd8ceb5eaa75092689ed0014533ec2df6
eycho <chw3872@gmail.com>
2023-06-26 15:21:26 +0900

    auto directory update

--- main.js
+++ main.js
@@ -7,11 +7,18 @@ var app = http.createServer(function (request, response) {
   var queryData = url.parse(_url, true).query;      //url을 분석하는 부분
   var pathname = url.parse(_url, true).pathname;
 
-  if(pathname === '/'){
-    if(queryData.id === undefined){
-      fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {    //(err, description) 함수 전달인자가 아니라 반환값
+  if (pathname === '/') {
+    if (queryData.id === undefined) {
+      fs.readdir('./data', function (error, filelist) {
         var title = 'Welcome';
         var description = 'Hello, Node.js';
+        var list = '<ul>';
+        var i = 0;
+        while (i < filelist.length) {
+          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          i++;
+        }
+        list = list + '</ul>';
         var template = `
         <!doctype html>
         <html>
@@ -21,23 +28,29 @@ var app = http.createServer(function (request, response) {
         </head>
         <body>
           <h1><a href="/">WEB</a></h1>
-          <ul>
-            <li><a href="/?id=HTML">HTML</a></li>
-            <li><a href="/?id=CSS">CSS</a></li>
-            <li><a href="/?id=JavaScript">JavaScript</a></li>
-          </ul>
+          ${list}
           <h2>${title}</h2>
           <p>${description}</p>
         </body>
         </html>
-        `;  
+        `;
         response.writeHead(200);    //200 : 성공적으로 접속에 성공했다를 전달
         response.end(template);
       });
-    }else{
-    fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
-      var title = queryData.id;
-      var template = `
+    } else {
+      fs.readdir('./data', function (error, filelist) {
+        var title = 'Welcome';
+        var description = 'Hello, Node.js';
+        var list = '<ul>';
+        var i = 0;
+        while (i < filelist.length) {
+          list += `<li><a href="/?id=${filelist[i]}">${filelist[i]}</a></li>`
+          i++;
+        }
+        list = list + '</ul>';
+        fs.readFile(`data/${queryData.id}`, 'utf-8', function (err, description) {
+          var title = queryData.id;
+          var template = `
       <!doctype html>
       <html>
       <head>
@@ -46,24 +59,22 @@ var app = http.createServer(function (request, response) {
       </head>
       <body>
         <h1><a href="/">WEB</a></h1>
-        <ul>
-          <li><a href="/?id=HTML">HTML</a></li>
-          <li><a href="/?id=CSS">CSS</a></li>
-          <li><a href="/?id=JavaScript">JavaScript</a></li>
-        </ul>
+        ${list}
         <h2>${title}</h2>
         <p>${description}</p>
       </body>
       </html>
-      `;  
-      response.writeHead(200);    //200 : 성공적으로 접속에 성공했다를 전달
-      response.end(template);
-    });
-  }}
-  else{
-      response.writeHead(404);
-      response.end('Not found');
+      `;
+          response.writeHead(200);    //200 : 성공적으로 접속에 성공했다를 전달
+          response.end(template);
+        });
+      });
+    }
   }
-  
+  else {
+    response.writeHead(404);
+    response.end('Not found');
+  }
+
 });
 app.listen(3000);
